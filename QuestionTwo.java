5e3fe99561ecbec87e4f0e146108e822f958a062
Preadator <nrish.ns@gmail.com>
2025-04-27 13:12:29 +0545

    Surprise Test

--- QuestionTwo.java
+++ QuestionTwo.java
@@ -1,16 +1,18 @@
 import java.util.Scanner;
 public class QuestionTwo {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        int userInput = sc.nextInt();
-
-        int sum = 0;
-        for (int i=0;i<=userInput;i++) {
-            sum +=i;
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter a number:");
+        int a=sc.nextInt();
+        int b=0;
+        while (a!=0){
+            int c=a%10;
+            b=b+c;
+            a=a/10;
         }
-        System.out.print(sum);
+        System.out.print("Sum="+b);
+        sc.close();
+
     }
 }
 
